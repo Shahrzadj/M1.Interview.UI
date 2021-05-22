cb1fcad890cfe57540983b468ca6c0f092ece182
sh.jahanbaz <shahrzadjahanbaz@gmail.com>
2021-05-22 17:07:24 +0430

    config months!

--- src/assets/js/showChart.js
+++ src/assets/js/showChart.js
@@ -11,22 +11,29 @@ var showChartModule = {
                 async: false,
                 contentType: "application/json; charset=utf-8",
                 success: function (response) {
+                    var sales = [12, 19, 3, 5, 2, 3, 12, 19, 3, 5, 2, 3];
                     $("#showChartModal").modal("toggle");
                     var ctx = document.getElementById('myChart').getContext('2d');
                     var myChart = new Chart(ctx, {
                         type: 'bar',
                         data: {
-                            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+                            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
                             datasets: [{
-                                label: '# of Votes',
-                                data: [12, 19, 3, 5, 2, 3],
+                                label: 'Sales Count',
+                                data: sales,
                                 backgroundColor: [
                                     'rgba(255, 99, 132, 0.2)',
                                     'rgba(54, 162, 235, 0.2)',
                                     'rgba(255, 206, 86, 0.2)',
                                     'rgba(75, 192, 192, 0.2)',
                                     'rgba(153, 102, 255, 0.2)',
-                                    'rgba(255, 159, 64, 0.2)'
+                                    'rgba(255, 159, 64, 0.2)',
+                                    'rgba(255, 99, 132, 0.2)',
+                                    'rgba(54, 162, 235, 0.2)',
+                                    'rgba(255, 206, 86, 0.2)',
+                                    'rgba(75, 192, 192, 0.2)',
+                                    'rgba(153, 102, 255, 0.2)',
+                                    'rgba(240, 170, 150, 0.5)'
                                 ],
                                 borderColor: [
                                     'rgba(255, 99, 132, 1)',
@@ -34,7 +41,13 @@ var showChartModule = {
                                     'rgba(255, 206, 86, 1)',
                                     'rgba(75, 192, 192, 1)',
                                     'rgba(153, 102, 255, 1)',
-                                    'rgba(255, 159, 64, 1)'
+                                    'rgba(255, 159, 64, 1)',
+                                    'rgba(255, 99, 132, 1)',
+                                    'rgba(54, 162, 235, 1)',
+                                    'rgba(255, 206, 86, 1)',
+                                    'rgba(75, 192, 192, 1)',
+                                    'rgba(153, 102, 255, 1)',
+                                    'rgba(240, 170, 150, 1)'
                                 ],
                                 borderWidth: 1
                             }]
