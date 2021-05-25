c06004e0c04157b954ee9dba25e7cc743efd71da
Shahrzad jahanbaz <shahrzadjahanbaz@gmail.com>
2021-05-26 01:37:41 +0430

    Change button style and add tooltip!

--- src/projectSettings/tableSpecifications.js
+++ src/projectSettings/tableSpecifications.js
@@ -8,18 +8,19 @@ let tableSetting = {
       data: null,
       render: function (data, meta) {
         return `
-         <button class="btn btn-sm btn-info showChart-button" data-id="${data.id}" data-row=${meta.row} onclick="showChartModule.fillInputs(${data.id},${meta.row})" type="button" >
-        <i class="fa fa-line-chart fa-lg" aria-hidden="true"></i>
-        </button>
-        <button  class="btn btn-sm btn-warning edit-button" data-id="${data.id}" data-row=${meta.row} onclick="editPersonModule.fillInputs(${data.id},${meta.row})" type="button" data-toggle="modal" data-target="#editpersonModal">
+            <button  class="btn btn-sm btn-warning edit-button" title="Edit" data-id="${data.id}" data-row=${meta.row} onclick="editPersonModule.fillInputs(${data.id},${meta.row})" type="button" data-toggle="modal" data-target="#editpersonModal">
             <i class="fa fa-pencil fa-lg" aria-hidden="true"></i>
             </button>
-            <button class="btn btn-sm btn-danger delete-button" data-id="${data.id}" data-row=${meta.row} onclick="deletePersonModule.fillInputs(${data.id},${meta.row})" type="button" data-toggle="modal" data-target="#deletePersonModal">
+            <button class="btn btn-sm btn-danger delete-button" title="Delete" data-id="${data.id}" data-row=${meta.row} onclick="deletePersonModule.fillInputs(${data.id},${meta.row})" type="button" data-toggle="modal" data-target="#deletePersonModal">
             <i class="fa fa-trash fa-lg" aria-hidden="true"></i>
             </button>   
-            <button class="btn btn-sm btn-success createSale-button" data-id="${data.id}" data-row=${meta.row} onclick="createSaleModule.fillInputs(${data.id},${meta.row})" type="button" data-toggle="modal" data-target="#addSaleModal">
+            |
+            <button class="btn btn-sm btn-success createSale-button" title="Add New Sale" data-id="${data.id}" data-row=${meta.row} onclick="createSaleModule.fillInputs(${data.id},${meta.row})" type="button" data-toggle="modal" data-target="#addSaleModal">
             <i class=" fa fa-plus-square fa-lg" aria-hidden="true"></i>
             </button>  
+            <button class="btn btn-sm btn-info showChart-button" title="Show Sales Chart" data-id="${data.id}" data-row=${meta.row} onclick="showChartModule.fillInputs(${data.id},${meta.row})" type="button" >
+            <i class="fa fa-line-chart fa-lg" aria-hidden="true"></i>
+            </button>    
            `;
       },
     },
